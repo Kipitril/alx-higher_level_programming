b43a6a44368307229542bddc42dc745cb9def91e
Kipitril <hezkipkorir@gmail.com>
2023-06-09 03:16:28 -0700

    list.h

---
+++ 0x03-python-data_structures/list.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_python_list_info(PyObject *p);
+int is_palindrome(listint_t **head);
+def delete_at(my_list=[], idx=0):
+def divisible_by_2(my_list=[]):
+def max_integer(my_list=[]):
+def multiple_returns(sentence):
+def add_tuple(tuple_a=(), tuple_b=()):
+def print_matrix_integer(matrix=[[]]):
+def no_c(my_string):
+def new_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
+def replace_in_list(my_list, idx, element):
+def element_at(my_list, idx):
+def print_list_integer(my_list=[]):
+
+#endif
